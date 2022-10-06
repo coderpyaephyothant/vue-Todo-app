10b4d34462e2492bb1b85c3e55fef2d03181347a
pyae phyo thant <pyaephyothantengr520@gmail.com>
2022-10-06 07:06:46 +0630

    Vue Cli Todo App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+     <h3 class="text-info text-center p-3 bg-secondary">
+        {{developer}}'s Todo App.
+     </h3>
+     <div class="container">
+      <div class="row pt-3">
+        <div class="col-6">
+          <h5 class="text-center text-secondary">Tasks</h5>
+        </div>
+        <div class="col-6">
+          <h5>
+            <div class="text-center text-info">Status</div>
+          </h5>
+        </div>
+      </div> <hr>
+      <div class="row p-3" v-for="(item,index) in tasks" :key="index">
+        <div class="col-6">
+          <h6 class="text-center text-secondary">{{item.toMake}}</h6>
+        </div>
+        <div class="col-6">
+          <h6>
+            <div class="text-center text-info">
+              <input type="checkbox" v-model="item.state">
+            </div>
+          </h6>
+        </div>
+      </div>
+     </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default{
+    name : "App",
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data : ()=>({
+      developer:'Pyae Phyo Thant',
+      tasks:[
+        { 'toMake':'Buy T-shirt','state':true },
+        { 'toMake':'Buy Flanel-shirt','state':true },
+        { 'toMake':'Buy Waterproof','state':false },
+        { 'toMake':'Buy Facewash','state':true },
+      ],
+    }),
+  };
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!-- <style>
+
+</style> -->
