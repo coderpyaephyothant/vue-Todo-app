f1cd1f9cb17258f01857c4f05252566c5521d13d
pyae phyo thant <pyaephyothantengr520@gmail.com>
2022-10-07 07:13:20 +0630

    todo app created

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,25 @@
         {{developer}}'s Todo App.
      </h3>
      <div class="container">
-      <div class="row pt-3">
+      <div class="row">
+        <div class="d-flex justify-content-around align-items-center mt-3">
+          <div>
+
+            <input type="text" @keyup.enter="newTask()" class="me-1 " placeholder="task name" v-model="taskName" name="" id="">
+            
+            <button class="btn btn-info btn-sm" @click="newTask()">Create</button>
+            <p class="text-danger" v-show="inputValidate">{{taskValidationText}}</p>
+          </div>
+          <div><span @click="ItemFilter" class="me-1 ">Hide Completed Task</span>
+            <input type="checkbox" name="" id="" v-model="hide"></div>
+        </div> 
+    </div> <hr>
+    <div class=" my-3 d-flex justify-content-between align-items-center">
+      <span>Total : {{this.tasks.length}}</span>
+      <button class="btn btn-sm btn-danger" @click="clear()">Tasks clear</button>
+    </div>
+    
+      <div class="row ">
         <div class="col-6">
           <h5 class="text-center text-secondary">Tasks</h5>
         </div>
@@ -13,10 +31,15 @@
             <div class="text-center text-info">Status</div>
           </h5>
         </div>
-      </div> <hr>
-      <div class="row p-3" v-for="(item,index) in tasks" :key="index">
+      </div> 
+
+      <div class="row" v-show="this.tasks.length == 0 ? 'dataCheck' : !'dataCheck'">
+        <div class="col-12 mt-3"><h6 class="text-danger text-center">No data to show here. ...</h6></div>
+      </div>
+      
+      <div class="row p-3" v-for="(item,index) in completedItemFilter" :key="index">
         <div class="col-6">
-          <h6 class="text-center text-secondary">{{item.toMake}}</h6>
+          <h6 class="text-center text-secondary " :class="item.state ? 'finishedSign': ''" >{{item.name}}</h6>
         </div>
         <div class="col-6">
           <h6>
@@ -26,6 +49,7 @@
           </h6>
         </div>
       </div>
+      
      </div>
   </div>
 </template>
@@ -36,17 +60,46 @@
 
     data : ()=>({
       developer:'Pyae Phyo Thant',
+      hide:false,
+      taskName:'',
+      taskValidationText:'where is your task name ?',
+      inputValidate:false,
+      dataCheck:true,
       tasks:[
-        { 'toMake':'Buy T-shirt','state':true },
-        { 'toMake':'Buy Flanel-shirt','state':true },
-        { 'toMake':'Buy Waterproof','state':false },
-        { 'toMake':'Buy Facewash','state':true },
+        // { 'toMake':'Buy T-shirt','state':true },
       ],
     }),
+
+    computed : {
+        completedItemFilter(){
+          return this.hide ? this.tasks.filter((v)=>v.state == false) : this.tasks;
+        }
+    },
+    methods : {
+      newTask(){
+//validate text 
+        this.dataCheck = false;
+        this.inputValidate = this.taskName == '' ? true : false;
+        if (this.inputValidate == false) {
+          this.tasks.push({
+          id:1,
+          name: this.taskName,
+          state:false,
+        });
+        this.taskName = '';
+        }
+      },
+      clear(){
+        this.tasks = [];
+
+      }
+    }
   };
 
 </script>
 
-<!-- <style>
-
-</style> -->
+<style>
+  .finishedSign{
+    text-decoration: line-through;
+  }
+</style>
